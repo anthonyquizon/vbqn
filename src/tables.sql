a736cf481885882286d51dff770dd8775c72ff7b
Anthony Quizon <anthoq88@gmail.com>
2024-11-04 14:35:04 +1100

    sql syntax check

--- src/tables.sql
+++ src/tables.sql
@@ -1,3 +1,9 @@
 -- 
-create table if not exists files (a string, b int, c double);
+create table if not exists bodies(id int primary key, filename string, call string, out string);
+
+create table if not exists vars(call int, name string, value string, foreign key(call) references bodies(id));
+-- create table if not exists vars_str(call int, name string, value string, foriegn key(call) REFERENCES bodies(id));
+-- create table if not exists vars_num(call int, name string, value int,    foriegn key(call) REFERENCES bodies(id));
+
+-- arrays? store bounds (min max) values?
 
