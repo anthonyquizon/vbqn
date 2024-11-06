7bd01826470408764eac2af1a41627b1a184fc72
Anthony Quizon <anthoq88@gmail.com>
2024-11-07 09:32:01 +1100

    do not duplicate refs

--- _test.sh
+++ _test.sh
@@ -1,11 +1,13 @@
 export TIMEFORMAT='%3R'
 
 find . -name '*.bqn' -o -name 'dbqn' -o -name '*.sql' -o -name '*.c' -o -name '*.test' | entr -r sh -c "
-time ./dbqn test/foo.bqn && \
+time ./dbqn test/foo.bqn -i test/input.bqn && \
     echo 'bodies'
     litecli _data/db <<< 'select * from bodies;exit'
-    echo 'vars'
-    litecli _data/db <<< 'select * from vars;exit'
+    echo 'refs'
+    litecli _data/db <<< 'select * from refs;exit'
+    echo 'vals'
+    litecli _data/db <<< 'select * from vals;exit'
     echo 'history'
     litecli _data/db <<< 'select * from history;exit'
 "
--- src/tables.sql
+++ src/tables.sql
@@ -2,7 +2,8 @@
 PRAGMA foreign_keys = ON;
 create table if not exists bodies(id int primary key, start int, file string);
 
-create table if not exists vars(id integer primary key autoincrement, body int, name string, line int, col int, foreign key(body) references bodies(id));
+create table if not exists refs(id integer primary key autoincrement, body int, name string, line int, col int, foreign key(body) references bodies(id));
+create table if not exists vals(id integer primary key autoincrement, ref int, i int, foreign key(ref) references refs(id));
 -- TODO calls
 
 -- create table if not exists vars  (body int, call int, name string, value string, reified boolean, foreign key(body) references bodies(id));
