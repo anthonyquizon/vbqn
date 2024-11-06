baba6a96f9e243f5cd285f89bc0267cd04804034
Anthony Quizon <anthoq88@gmail.com>
2024-11-06 15:05:41 +1100

    directly inject into sqlite

--- src/tables.sql
+++ src/tables.sql
@@ -1,10 +1,10 @@
 -- 
 PRAGMA foreign_keys = ON;
-create table if not exists bodies(id int primary key, start int, filename string);
+create table if not exists bodies(id int primary key, start int, file string);
 
-create table if not exists vars  (body int, call int, name string, value string, reified boolean, foreign key(body) references bodies(id));
-create table if not exists regs  (body int, name string, col_s int, col_e int, line int, foreign key(body) references bodies(id));
-create table if not exists output(body int, call int, name string, value string, foreign key(body) references bodies(id));
+-- create table if not exists vars  (body int, call int, name string, value string, reified boolean, foreign key(body) references bodies(id));
+create table if not exists regs  (body int, name string, line int, col int, foreign key(body) references bodies(id));
+-- create table if not exists output(body int, call int, name string, value string, foreign key(body) references bodies(id));
 
 create table if not exists history(i integer primary key autoincrement, body int, foreign key(body) references bodies(id));
 
