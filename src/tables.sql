40509edb62e73c9d784bde790dd51c5e6f96ab4b
Anthony Quizon <anthoq88@gmail.com>
2024-11-07 17:21:34 +1100

    fix: duplicate refs

--- src/tables.sql
+++ src/tables.sql
@@ -1,6 +1,6 @@
 -- 
 PRAGMA foreign_keys = ON;
-create table if not exists bodies(id int primary key, start int, file string);
+create table if not exists bodies(id int primary key, line int, col int, file string);
 
 create table if not exists refs(id integer primary key autoincrement, body int, name string, line int, col int, foreign key(body) references bodies(id));
 create table if not exists vals(id integer primary key autoincrement, ref int, i int, type int, foreign key(ref) references refs(id));
