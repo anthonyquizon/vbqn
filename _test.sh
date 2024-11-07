a1033ca79293237a87821e9462cf19583613df9b
Anthony Quizon <anthoq88@gmail.com>
2024-11-07 21:38:03 +1100

    sqlite3 output

--- _test.sh
+++ _test.sh
@@ -1,13 +1,21 @@
 export TIMEFORMAT='%3R'
 
+sql="
+.mode column
+.headers on
+.print 'bodies'
+select * from bodies;
+
+.print 'refs'
+select * from refs;
+
+.print 'vals'
+select * from vals;
+
+.print 'history'
+select * from history;
+"
+
 find . -name '*.bqn' -o -name 'dbqn' -o -name '*.sql' -o -name '*.c' -o -name '*.test' | entr -r sh -c "
-time ./dbqn test/foo.bqn -i test/input.bqn && \
-    echo 'bodies'
-    litecli _data/db <<< 'select * from bodies;exit'
-    echo 'refs'
-    litecli _data/db <<< 'select * from refs;exit'
-    echo 'vals'
-    litecli _data/db <<< 'select * from vals;exit'
-    echo 'history'
-    litecli _data/db <<< 'select * from history;exit'
+time (./dbqn test/foo.bqn -i test/input.bqn && sqlite3 _data/db <<< \"$sql\")
 "
