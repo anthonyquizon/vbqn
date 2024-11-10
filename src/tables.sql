7e1cba9e7858086272ac3931b43968032ab6c567
Anthony Quizon <anthoq88@gmail.com>
2024-11-11 08:49:48 +1100

    bash entry point with symlink pwd

--- src/tables.sql
+++ src/tables.sql
@@ -1,4 +1,3 @@
--- 
 PRAGMA foreign_keys = ON;
 create table if not exists bodies(id int primary key, line int, col int, file string);
 
@@ -6,9 +5,6 @@ create table if not exists refs(id integer primary key autoincrement, body int, 
 create table if not exists vals(id integer primary key autoincrement, ref int, i int, type int, foreign key(ref) references refs(id));
 -- TODO calls
 
--- create table if not exists vars  (body int, call int, name string, value string, reified boolean, foreign key(body) references bodies(id));
--- create table if not exists output(body int, call int, name string, value string, foreign key(body) references bodies(id));
-
-create table if not exists history(i integer primary key autoincrement, body int, foreign key(body) references bodies(id));
+create table if not exists history(i integer primary key autoincrement, body int, depth int, foreign key(body) references bodies(id));
 
 
