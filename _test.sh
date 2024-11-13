c0d198204fb139619489e342b753ad2b8c1f78fa
Anthony Quizon <anthoq88@gmail.com>
2024-11-13 11:13:35 +1100

    compiler error messages

--- _test.sh
+++ _test.sh
@@ -12,10 +12,13 @@ select * from refs;
 .print 'vals'
 select * from vals;
 
+.print 'ref vals'
+select name, type, line, col from vals inner join refs on refs.id = vals.ref;
+
 .print 'history'
 select * from history;
 "
 
 find . -name '*.bqn' -o -name 'fern' -o -name '*.sql' -o -name '*.c' -o -name '*.test' | entr -r sh -c "
-time (./fern test/_tmp/foo.bqn -i test/input.bqn && sqlite3 _data/db <<< \"$sql\")
+time (./fern test/_tmp/e2.bqn -i test/input.bqn && sqlite3 _data/db <<< \"$sql\")
 "
