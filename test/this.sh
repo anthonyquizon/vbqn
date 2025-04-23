281fa4cbbf6430649dcc1fd6fe422ffd2fe89a32
Anthony Quizon <anthoq88@gmail.com>
2025-04-24 09:43:01 +1000

    test with --expect flag

--- test/this.sh
+++ test/this.sh
@@ -1,4 +1,10 @@
 #! /usr/bin/env bash
 cd "$(dirname "$0")"
 export TIMEFORMAT='%3R'
-time diff <(../vbqn -v v.bqn a.bqn) expect && echo -e "\e[32mPassed\e[0m" || echo -e "\e[31mFail\e[0m"
+
+if [[ "$@" == *"--expect"* ]]; then
+    time diff <(../vbqn -v v.bqn a.bqn) expect \
+        && echo -e "\e[32mPassed\e[0m" || echo -e "\e[31mFail\e[0m"
+else 
+    time ../vbqn -v v.bqn a.bqn
+fi
